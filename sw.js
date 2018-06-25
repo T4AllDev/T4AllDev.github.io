787fe97324e7f3585f83935f18d913a58d1a5c04
Gilberto <gitkraken2403@mailnesia.com>
2018-06-25 15:54:40 +0200

    Modfificato parte relativa al Service Worker

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
 /** An empty service worker! */
+self.addEventListener('install', function (event) {
+    // Perform install steps
+});
 self.addEventListener('fetch', function (event) {
     /** An empty fetch handler! */
 });
